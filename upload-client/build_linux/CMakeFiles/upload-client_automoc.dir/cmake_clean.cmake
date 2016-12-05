c3b9913a2598014c835b6508c63c98e2a43cb0f7
Petro <petro@petro.ws>
2016-12-05 10:43:41 +0300

    cmake build

--- upload-client/build_linux/CMakeFiles/upload-client_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "upload-client_automoc.cpp"
-  "CMakeFiles/upload-client_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/upload-client_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
