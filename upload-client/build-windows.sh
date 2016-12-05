f76e06fa8174a841d4278888faedd54324b0dba2
Petro <petro@petro.ws>
2016-12-05 14:08:50 +0300

    build fix

--- upload-client/build-windows.sh
+++ upload-client/build-windows.sh
@@ -3,6 +3,8 @@
 # Exit on error
 set -e 
 
+export PATH=/bin:$PATH
+
 QT_DIR="C:\\Qt\\5.7\\mingw53_32"
 MINGW_DIR="c:\\Qt\\Tools\\mingw530_32"
 
@@ -16,15 +18,19 @@ MINGW_BIN=$(/bin/cygpath -u "$MINGW_DIR")"/bin"
 export PATH=$MINGW_BIN:$PATH
 export QTDIR="$MINGW_QT"
 
-/bin/rm -rf build_win32
-/bin/mkdir build_win32
+rm -rf build_win32
+mkdir build_win32
 cd build_win32
-/bin/mkdir bin
+mkdir bin
 cmake.exe -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX="./" -DCMAKE_BUILD_TYPE=Release ..
 "$MINGW_BIN/mingw32-make.exe" all install -j4
 
 cd bin
 "$MINGW_QT/windeployqt.exe" --release *.exe
 
-# Make installer 
+# Make portable installer
+zip -r -9 $ROOT_PATH/../upload-client.zip *
+
+# Make exe installer 
 makensis.exe /NOCD $(/bin/cygpath -w $ROOT_PATH/installer.nsi)
+mv upload-client-setup.exe $ROOT_PATH/../upload-client-setup.exe
