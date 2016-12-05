9104c40b2e629677aa075bcb99173349ce1c45f4
Petro <petro@petro.ws>
2016-12-05 13:26:46 +0300

    mingw build

--- upload-client/build-windows.sh
+++ upload-client/build-windows.sh
@@ -3,24 +3,24 @@
 # Exit on error
 set -e 
 
-QT_DIR="C:\\Qt\\5.7\\msvc2015"
-COMPILER_DIR="C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\bin"
+QT_DIR="C:\\Qt\\5.7\\mingw53_32"
+MINGW_DIR="c:\\Qt\\Tools\\mingw530_32"
 
 export CMAKE_PREFIX_PATH="$QT_DIR"
 
-QT_BIN=$(/bin/cygpath -u "$QT_DIR")"/bin"
-COMPILER_BIN=$(/bin/cygpath -u "$COMPILER_DIR")"/bin"
+MINGW_QT=$(/bin/cygpath -u "$QT_DIR")"/bin"
+MINGW_BIN=$(/bin/cygpath -u "$MINGW_DIR")"/bin"
 
-export PATH=$COMPILER_BIN:$PATH
-export QTDIR="$QT_BIN"
+export PATH=$MINGW_BIN:$PATH
+export QTDIR="$MINGW_QT"
 
 /bin/rm -rf build_win32
 /bin/mkdir build_win32
 cd build_win32
 /bin/mkdir bin
-cmake.exe -G "Visual Studio 14" -DCMAKE_INSTALL_PREFIX="./" -DCMAKE_BUILD_TYPE=Release ..
-MSBuild.exe /p:Configuration=Release upload-client.sln
+cmake.exe -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX="./" -DCMAKE_BUILD_TYPE=Release ..
+"$MINGW_BIN/mingw32-make.exe" all install -j4
 
 cd bin
-"$QT_BIN/windeployqt.exe" --release *.exe
+"$MINGW_QT/windeployqt.exe" --release *.exe
 
